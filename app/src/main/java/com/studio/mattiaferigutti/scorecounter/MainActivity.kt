b15336ab36185352f83b9106468da5b620cc9d7d
Mattia Ferigutti <mattiaferigutti@gmail.com>
2020-12-16 18:32:50 +0100

    implementing ViewModel to save data

--- app/src/main/java/com/studio/mattiaferigutti/scorecounter/MainActivity.kt
+++ app/src/main/java/com/studio/mattiaferigutti/scorecounter/MainActivity.kt
@@ -3,12 +3,12 @@ package com.studio.mattiaferigutti.scorecounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.viewModels
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var currentScoreTeamA = 0
-    private var currentScoreTeamB = 0
+    private val viewModel: MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,18 +29,38 @@ class MainActivity : AppCompatActivity() {
         removeTeamBButton.setOnClickListener {
             removePoint(it.id)
         }
+
+        updateTextView(teamATextView, viewModel.currentScoreTeamA)
+        updateTextView(teamBTextView, viewModel.currentScoreTeamB)
     }
+
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//
+//        outState.putInt(SCORE_TEAM_A, currentScoreTeamA)
+//        outState.putInt(SCORE_TEAM_B, currentScoreTeamB)
+//    }
+//
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//
+//        currentScoreTeamA = savedInstanceState.getInt(SCORE_TEAM_A, 0)
+//        currentScoreTeamB = savedInstanceState.getInt(SCORE_TEAM_B, 0)
+//
+//        updateTextView(teamATextView, currentScoreTeamA)
+//        updateTextView(teamBTextView, currentScoreTeamB)
+//    }
 
     private fun addPoint(id: Int) {
         when (id) {
             R.id.addTeamAButton -> {
-                currentScoreTeamA++
-                updateTextView(teamATextView, currentScoreTeamA)
+                viewModel.currentScoreTeamA++
+                updateTextView(teamATextView, viewModel.currentScoreTeamA)
             }
 
             R.id.addTeamBButton -> {
-                currentScoreTeamB++
-                updateTextView(teamBTextView, currentScoreTeamB)
+                viewModel.currentScoreTeamB++
+                updateTextView(teamBTextView, viewModel.currentScoreTeamB)
             }
         }
     }
@@ -48,13 +68,13 @@ class MainActivity : AppCompatActivity() {
     private fun removePoint(id: Int) {
         when (id) {
             R.id.removeTeamAButton -> {
-                currentScoreTeamA--
-                updateTextView(teamATextView, currentScoreTeamA)
+                viewModel.currentScoreTeamA--
+                updateTextView(teamATextView, viewModel.currentScoreTeamA)
             }
 
             R.id.removeTeamBButton -> {
-                currentScoreTeamB--
-                updateTextView(teamBTextView, currentScoreTeamB)
+                viewModel.currentScoreTeamB--
+                updateTextView(teamBTextView, viewModel.currentScoreTeamB)
             }
         }
     }
@@ -63,4 +83,8 @@ class MainActivity : AppCompatActivity() {
         textView.text = score.toString()
     }
 
+    companion object {
+        val SCORE_TEAM_A = MainActivity::class.java.name + ".SCORE_TEAM_A"
+        val SCORE_TEAM_B = MainActivity::class.java.name + ".SCORE_TEAM_B"
+    }
 }
