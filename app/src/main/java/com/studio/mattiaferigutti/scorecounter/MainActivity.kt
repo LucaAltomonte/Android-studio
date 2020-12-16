8f3ec7271e3341bbb92c67dcecdbcc991513f16d
Mattia Ferigutti <mattiaferigutti@gmail.com>
2020-12-16 18:27:51 +0100

    implementing onSavedInstance to save data

--- app/src/main/java/com/studio/mattiaferigutti/scorecounter/MainActivity.kt
+++ app/src/main/java/com/studio/mattiaferigutti/scorecounter/MainActivity.kt
@@ -2,14 +2,13 @@ package com.studio.mattiaferigutti.scorecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.TextView
-import androidx.activity.viewModels
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainActivityViewModel by viewModels()
+    private var currentScoreTeamA = 0
+    private var currentScoreTeamB = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,38 +29,35 @@ class MainActivity : AppCompatActivity() {
         removeTeamBButton.setOnClickListener {
             removePoint(it.id)
         }
-
-        updateTextView(teamATextView, viewModel.currentScoreTeamA)
-        updateTextView(teamBTextView, viewModel.currentScoreTeamB)
     }
 
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//
-//        outState.putInt(SCORE_TEAM_A, viewModel.currentScoreTeamA)
-//        outState.putInt(SCORE_TEAM_B, viewModel.currentScoreTeamB)
-//    }
-//
-//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-//        super.onRestoreInstanceState(savedInstanceState)
-//
-//        viewModel.currentScoreTeamA = savedInstanceState.getInt(SCORE_TEAM_A, 0)
-//        viewModel.currentScoreTeamB = savedInstanceState.getInt(SCORE_TEAM_B, 0)
-//
-//        updateTextView(teamATextView, viewModel.currentScoreTeamA)
-//        updateTextView(teamBTextView, viewModel.currentScoreTeamB)
-//    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_TEAM_A, currentScoreTeamA)
+        outState.putInt(SCORE_TEAM_B, currentScoreTeamB)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        currentScoreTeamA = savedInstanceState.getInt(SCORE_TEAM_A, 0)
+        currentScoreTeamB = savedInstanceState.getInt(SCORE_TEAM_B, 0)
+
+        updateTextView(teamATextView, currentScoreTeamA)
+        updateTextView(teamBTextView, currentScoreTeamB)
+    }
 
     private fun addPoint(id: Int) {
         when (id) {
             R.id.addTeamAButton -> {
-                viewModel.currentScoreTeamA++
-                updateTextView(teamATextView, viewModel.currentScoreTeamA)
+                currentScoreTeamA++
+                updateTextView(teamATextView, currentScoreTeamA)
             }
 
             R.id.addTeamBButton -> {
-                viewModel.currentScoreTeamB++
-                updateTextView(teamBTextView, viewModel.currentScoreTeamB)
+                currentScoreTeamB++
+                updateTextView(teamBTextView, currentScoreTeamB)
             }
         }
     }
@@ -69,18 +65,18 @@ class MainActivity : AppCompatActivity() {
     private fun removePoint(id: Int) {
         when (id) {
             R.id.removeTeamAButton -> {
-                viewModel.currentScoreTeamA--
-                updateTextView(teamATextView, viewModel.currentScoreTeamA)
+                currentScoreTeamA--
+                updateTextView(teamATextView, currentScoreTeamA)
             }
 
             R.id.removeTeamBButton -> {
-                viewModel.currentScoreTeamB--
-                updateTextView(teamBTextView, viewModel.currentScoreTeamB)
+                currentScoreTeamB--
+                updateTextView(teamBTextView, currentScoreTeamB)
             }
         }
     }
 
-    private fun updateTextView(textView: TextView, score: Int?) {
+    private fun updateTextView(textView: TextView, score: Int) {
         textView.text = score.toString()
     }
 
